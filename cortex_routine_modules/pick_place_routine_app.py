d5c0f90087d704c66899ffd6716fef3a4ed485bd
Labeeb Nassar <labeebranassar@gmail.com>
2025-05-12 17:59:32 +0530

    updated_MAY_12

--- cortex_routine_modules/pick_place_routine_app.py
+++ cortex_routine_modules/pick_place_routine_app.py
@@ -12,6 +12,7 @@ from typing import Optional
 from isaacsim.core.prims import RigidPrim #type:ignore
 from pxr import Usd #type:ignore
 
+from isaacsim.core.api.objects import VisualCuboid #type:ignore
 import isaacsim.cortex.framework.math_util as math_utils #type:ignore
 from isaacsim.cortex.framework.motion_commander import ApproachParams, PosePq #type:ignore
 from isaacsim.cortex.framework.cortex_world import CortexWorld #type:ignore
@@ -19,29 +20,35 @@ from isaacsim.cortex.framework.robot import add_ur10_to_stage #type:ignore
 from isaacsim.cortex.framework.df import DfNetwork, DfState, DfStateMachineDecider, DfStateSequence, DfWaitState, DfSetLockState #type:ignore
 from isaacsim.cortex.framework.dfb import DfBasicContext, DfRobotApiContext, DfDiagnosticsMonitor #type:ignore
 
-
-def vis_debug_callback(step_size, bot_collision_vis=False):
-    if bot_collision_vis:
-        robot.motion_policy.visualize_collision_spheres()
-
+# ----------------------------------- DIAGNOSTICS MONITOR FOR THE LINE ------------------------------------
 class PickPlaceDiagnosticsMonitor(DfDiagnosticsMonitor):
     def __init__(self, print_dt=1.0, diagnostic_fn=None):
         super().__init__(print_dt=print_dt)
         self.diagnostic_fn = diagnostic_fn
 
     def print_diagnostics(self, context):
-        print(context.joint_info)
+        # print(context.joint_info)
         print(context.eef_info)
 
-
+# ---------------------------------------------------------------------------------------------------------
 class PickPlaceContext(DfRobotApiContext):
-    def __init__(self, robot, obj: Optional[RigidPrim], p_thresh: float=0.003, r_thresh: float=1.0, monitor_fn=None):
+    def __init__(
+        self, 
+        robot, 
+        obj: Optional[RigidPrim], 
+        p_thresh: float=0.003, 
+        r_thresh: float=1.0,  
+        monitor_fn=None
+    ):
         super().__init__(robot)
         self.reset()
 
+
+        self.home = False # temp flag
+
         self.obj = obj
         self.p_thresh, self.r_thresh = p_thresh, r_thresh
-        self.target_pos, self.target_q = None, None
+        self.target_pos, self.target_q = None, None        
 
         self.pp_diagnostics_monitor = PickPlaceDiagnosticsMonitor(print_dt=0.1, diagnostic_fn=monitor_fn)
 
@@ -78,10 +85,10 @@ class PickPlaceContext(DfRobotApiContext):
         errR, errP = math_utils.unpack_T(errT)
         nerrR = np.linalg.norm(errR)
 
-        # self.is_target_reached = math_utils.transforms_are_close(
-        #     T1=eef_pose, T2=target_T, p_thresh=self.p_thresh, R_thresh=self.r_thresh
-        # )
-        self.is_target_reached = np.linalg.norm(eef_pos-self.target_pos) < self.p_thresh
+        self.is_target_reached = math_utils.transforms_are_close(
+            T1=eef_pose, T2=target_T, p_thresh=self.p_thresh, R_thresh=self.r_thresh
+        )
+        # self.is_target_reached = np.linalg.norm(eef_pos-self.target_pos) < self.p_thresh
 
         self.eef_info = {
             "current eef pose (FK_T)": eef_pose, 
@@ -91,7 +98,7 @@ class PickPlaceContext(DfRobotApiContext):
             "target reached": self.is_target_reached,
             "grasped": self.is_grasped
         }
-
+                
     def monitor_joint_info(self):
         joint_positions = self.robot_art.get_joint_positions()
         num_active_joints = self.robot.arm.num_controlled_joints
@@ -100,13 +107,40 @@ class PickPlaceContext(DfRobotApiContext):
             "joint_position(rad)": joint_positions,
             "joint_position(deg)": joint_positions*(180/np.pi),
         }
+    
+# ------------------------------------------------------------------------------------------------
+class GoToHome(DfState):
+    def __init__(self):
+        super().__init__()
+        self.home = False
+        self.home_pose = None
+    
+    @property
+    def robot(self):
+        return self.context.robot
+
+    def enter(self):
+        print("<Go To Home>")
+        initial_joint_config = np.array([-1.57, -1.57, -1.57, -1.57,  1.57,  0])
+        # FK output 
+        self.home_pose = self.robot.arm.get_fk_pq(config=initial_joint_config)
+        self.robot.arm.send_end_effector(
+            target_pose=self.home_pose, posture_config=initial_joint_config
+        )
+
+    def step(self):
+        eef_p, _ = math_utils.T2pq(self.robot.arm.get_fk_T())
+        home_p, _ = self.home_pose
+        self.home = np.linalg.norm(home_p-eef_p) < 0.003 # 3mm accuracy
+        if self.home:
+            return None
         
+        return self
+
 class ReachToPick(DfState):
     def __init__(self):
         super().__init__()
         self.entry_time = None
-
-        print("<Sample Test State>")
 
     @property
     def robot(self):
@@ -120,14 +154,16 @@ class ReachToPick(DfState):
         return self.robot.arm.motion_policy.get_default_cspace_position_target()
 
     def enter(self):
+        print("<Reach To Pick>")
         self.entry_time = time.time()
-        print(f"starting end effector pose: {self.robot.arm.get_end_effector_pose()}\n")
-        print(f"starting joint positions: {self.robot_art.get_joint_positions()}\n")
         print(f"setting target pos(p) and orientation(q) as: [{self.context.target_pos}--{self.context.target_q}]")
-
+        print(f"initial end effector pose: {self.robot.arm.get_end_effector_pose()}\n")
+        print(f"initial joint positions: {self.robot_art.get_joint_positions()}\n")
+        
     def step(self):
-        approach_params = ApproachParams(direction=0.3*np.array([0.0, 0.0, -1.0]), std_dev=0.04)
-        posture_config = np.array([-1.57, -1.57, -1.57, -1.57, 1.57, -3.4733276e-08])
+        approach_params = ApproachParams(direction=0.3*np.array([0.0, 0.0, -1.0]), std_dev=0.005)
+        # posture_config = np.array([-1.57, -1.57, -1.57, -1.57, 1.57, -3.4733276e-08])
+        posture_config = self.robot.default_config
         self.robot.arm.send_end_effector(
             target_position=self.context.target_pos, approach_params=approach_params, posture_config=posture_config
         )
@@ -136,7 +172,38 @@ class ReachToPick(DfState):
         
         return self
 
+class ReachToPlace(DfState):
+    def __init__(self):
+        super().__init__()
+        self.entry_time = None
 
+    @property
+    def robot(self):
+        return self.context.robot
+    
+    @property
+    def robot_art(self):
+        return self.robot.arm.articulation_subset
+    
+    def get_posture_config(self):
+        return self.robot.arm.motion_policy.get_default_cspace_position_target()
+    
+    def enter(self):
+        print("<Reach To Place>")
+        # hardcoded placement position
+
+# ------------------------------------- end effector tool states -------------------------------
+class CloseSuctionGripperWithRetries(DfState):
+    def enter(self):
+        pass
+        
+    def step(self):
+        gripper = self.robot.suction_gripper
+        gripper.close()
+        if gripper.is_close():
+            return None
+        return self
+    
 class CloseSuctionGripper(DfState):
     def enter(self):
         print("<close gripper>")
@@ -150,64 +217,90 @@ class OpenSuctionGripper(DfState):
         self.context.robot.suction_gripper.open()
     def step(self):
         return None
+    
+# ------------------------------------------- HELPER STATES ----------------------------------------
+class DoNothing(DfState):
+    def enter(self):
+        self.context.robot.arm.clear()
+
+    def step(self):
+        print(self.context.robot.arm.target_prim.get_world_pose())
+        return self
+
+# --------------------------------------------------------------------------------------------------
+
+# --------------- STATE MACHINE DECIDER NODES (future implementation; for better functionality) -----
+# --------------------------------------------------------------------------------------------------
 
 
-# ------------ loading a custom USD scene into the cortex world system ----------------
-working_dir = os.path.join("/home/inlabust/labeeb/warehouse_sap")
-usd_file_path = os.path.join(working_dir, 'isolated_cortex_test_env.usd')
-usd_context = omni.usd.get_context()
-usd_context.open_stage(usd_file_path)
-stage = usd_context.get_stage()
 
-if stage is None:
-    raise RuntimeError(f"Failed to open USD stage at {usd_file_path}")
+def main():
 
-if not stage.GetDefaultPrim():
-    stage.SetDefaultPrim(stage.GetPrimAtPath("/World"))
-    print("Default prim set to /World.")
+    def vis_debug_callback(step_size, bot_collision_vis=False):
+        if bot_collision_vis:
+            robot.motion_policy.visualize_collision_spheres()
 
-physics_path = "/World/PhysicsScene"
-# ----------------------------------- WORLD AND BOT SETUP --------------------------------------------------
-world = CortexWorld(
-    physics_dt=1/60,
-    rendering_dt=1/60,
-    stage_units_in_meters=1.0,
-    physics_prim_path=physics_path
-)
-robot = world.add_robot(
-    add_ur10_to_stage(
-        name='ur10',
-        prim_path="/World/ur10_suction"
+    # ------------ loading a custom USD scene into the cortex world system ----------------
+    working_dir = os.path.join("/home/inlabust/labeeb/warehouse_sap")
+    usd_file_path = os.path.join(working_dir, 'isolated_cortex_test_env.usd')
+    usd_context = omni.usd.get_context()
+    usd_context.open_stage(usd_file_path)
+    stage = usd_context.get_stage()
+
+    if stage is None:
+        raise RuntimeError(f"Failed to open USD stage at {usd_file_path}")
+
+    if not stage.GetDefaultPrim():
+        stage.SetDefaultPrim(stage.GetPrimAtPath("/World"))
+        print("Default prim set to /World.")
+
+    physics_path = "/World/PhysicsScene"
+    # ----------------------------------- WORLD AND BOT SETUP --------------------------------------------------
+    world = CortexWorld(
+        physics_dt=1/60,
+        rendering_dt=1/60,
+        stage_units_in_meters=1.0,
+        physics_prim_path=physics_path
     )
-)
+    robot = world.add_robot(
+        add_ur10_to_stage(
+            name='ur10',
+            prim_path="/World/ur10_suction"
+        )
+    )
 
-obj_prim_path = '/World/Cardbox_B2'
-cb_prim = stage.GetPrimAtPath(obj_prim_path)
-if not cb_prim.IsValid():
-    raise RuntimeError(f"{obj_prim_path} is not a valid prim path")
+    obj_prim_path = '/World/Cardbox_B2'
+    cb_prim = stage.GetPrimAtPath(obj_prim_path)
+    if not cb_prim.IsValid():
+        raise RuntimeError(f"{obj_prim_path} is not a valid prim path")
 
-obj = RigidPrim(
-    prim_paths_expr=obj_prim_path,
-    name='cb_rigid_view'
-)
+    obj = RigidPrim(
+        prim_paths_expr=obj_prim_path,
+        name='cb_rigid_view'
+    )
 
-# --------------------- adding the behaviour/decider networks here ----------------------
-decider_network = DfNetwork(
-    DfStateMachineDecider(
-        DfStateSequence(
-            [   
-                ReachToPick(),
-                DfWaitState(wait_time=0.5),
-                CloseSuctionGripper()
-            ], 
-        
-        loop=True)),
-    context=PickPlaceContext(robot, obj)
-)
-world.add_decider_network(decider_network)
-# ---------------------------------------------------------------------------------------
-world.run(simulation_app)
-simulation_app.close()
+    # --------------------- adding the behaviour/decider networks here ----------------------
+    decider_network = DfNetwork(
+        DfStateMachineDecider(
+            DfStateSequence(
+                [ 
+                    ReachToPick(),
+                    # DfWaitState(wait_time=2.0),
+                    # ReachToPlace(),
+                    # DoNothing(),
+                ], 
+            
+            loop=True)),
+        context=PickPlaceContext(robot, obj)
+    )
+    world.add_decider_network(decider_network)
+
+    # ------------------------------------ RUNNING SIMULATION APP ----------------------------
+    world.run(simulation_app)
+    simulation_app.close()
+
+if __name__ == '__main__':
+    main()
 
 
 
